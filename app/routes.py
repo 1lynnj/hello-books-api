7bd8acd47a053d088a793347d1e855096905c3f6
Lynn <lynn@jansheski.com>
2022-10-19 16:25:41 -0700

    created app and models directories, created routes.py and wrote test endpoints

---
+++ app/routes.py
@@ -0,0 +1,16 @@
+from flask import Blueprint
+
+hello_world_bp = Blueprint("hello_world", __name__)
+
+
+# example of an endpoint
+@hello_world_bp.route("/hello-world", methods=["GET"])
+def say_hello_world():
+    my_beautiful_response_body = "Hello, World!"
+    return my_beautiful_response_body
+
+@hello_world_bp.route("/hello/JSON", methods=["GET"])
+def say_hello_json():
+    return {
+        "name": "Ada Lovelace",
+    }
